d66dc66eff677ea17514b9054259af13a48a85ec
viral <viral.patel218000@gmail.com>
2021-05-29 03:37:10 +0530

    29/5/21 commit

--- Java/viral8.java
+++ Java/viral8.java
@@ -1,7 +1,27 @@
+
+
+// check data type of variable
+
 public class viral8 {
     public static void main(String[] args) {
-        
-        int number = 57;
-        System.out.println(number>>2);
+
+        byte b = 3;
+        short s = 7;
+        int i = 13;
+        long l = 1317;
+        float f = 13.17f;
+        double d = 1313.1717d;
+        boolean bl = true;
+        char c = 'E';
+
+        System.out.println(Byte.SIZE);
+        System.out.println(Short.SIZE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Long.SIZE);
+        System.out.println(Float.SIZE);
+        System.out.println(Double.SIZE);
+        System.out.println(Boolean.class);
+        System.out.println(Character.SIZE);
+
     }
 }
