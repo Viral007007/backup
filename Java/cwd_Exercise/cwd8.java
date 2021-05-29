b72e66638fedf013fb3f94b9181213ee6e277f2f
viral <viral.patel218000@gmail.com>
2021-05-30 01:19:31 +0530

    30/5/21 commit

---
+++ Java/cwd_Exercise/cwd8.java
@@ -0,0 +1,15 @@
+// practice set 2 ; Question 2 : encrypt a grade by adding 8 to it. decrypy it to show the correct grade
+
+
+public class cwd8 {
+    public static void main(String[] args) {
+        //encypt grade
+        char grade = 'A';
+        grade = (char)(grade + 8);
+        System.out.println(grade); 
+
+        grade = (char)(grade - 8);
+        System.out.println(grade); 
+
+    }    
+}
